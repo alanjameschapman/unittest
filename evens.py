8b05e6fa29145707195e6fa549e7aca756324420
Alan <alanjameschapman@gmail.com>
2024-01-17 20:29:37 +0000

    Add implementation for even_number_of_evens function and test cases

--- evens.py
+++ evens.py
@@ -7,7 +7,17 @@ def even_number_of_evens(numbers):
     if the number of even numbers is 0, it should return False
     if the number of even numbers is even, it should return True
     """
-    return None
+    if isinstance(numbers, list):
+        evens = 0
+        for number in numbers:
+            if number % 2 == 0:
+                evens += 1
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
+    else:
+        raise TypeError("A list was not passed into the function")
 
 if __name__ == "__main__":
     print(even_number_of_evens(5))
